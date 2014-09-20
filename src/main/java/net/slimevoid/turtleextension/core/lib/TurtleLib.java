087544881b5f8a4eb189088ba0fe2d765f41adbb
Eurymachus <reflex_ion@hotmail.com>
2014-09-20 16:08:48 +0100

    Language

--- src/main/java/net/slimevoid/turtleextension/core/lib/TurtleLib.java
+++ src/main/java/net/slimevoid/turtleextension/core/lib/TurtleLib.java
@@ -2,7 +2,6 @@ package net.slimevoid.turtleextension.core.lib;
 
 public class TurtleLib {
 
-	public static final String SHEAR_UPGRADE = "slimevoid.turtle.upgrade.shear";
-	public static final String SHEAR_PERIPHERAL = "slimevoid.turtle.peripheral.shear";
+	public static final String SHEAR_UPGRADE = "slimevoid.turtle.upgrade.shears";
 
 }
